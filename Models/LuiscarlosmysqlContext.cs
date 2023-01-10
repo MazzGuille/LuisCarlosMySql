600c4e01bbee5be8fa53675b8c29e90a8ae8aedc
MazzCode <mazzcode@gmail.com>
2023-01-10 12:55:03 -0300

    Add project files.

---
+++ Models/LuiscarlosmysqlContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace LuisCarlosMySql.Models;
+
+public partial class LuiscarlosmysqlContext : DbContext
+{
+    public LuiscarlosmysqlContext()
+    {
+    }
+
+    public LuiscarlosmysqlContext(DbContextOptions<LuiscarlosmysqlContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Producto> Productos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8mb4_0900_ai_ci")
+            .HasCharSet("utf8mb4");
+
+        modelBuilder.Entity<Producto>(entity =>
+        {
+            entity.HasKey(e => e.IdProductos).HasName("PRIMARY");
+
+            entity.ToTable("productos");
+
+            entity.Property(e => e.IdProductos).HasColumnName("idProductos");
+            entity.Property(e => e.Marca).HasMaxLength(45);
+            entity.Property(e => e.Modelo).HasMaxLength(45);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Models/Producto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LuisCarlosMySql.Models;
+
+public partial class Producto
+{
+    public int IdProductos { get; set; }
+
+    public string Marca { get; set; } = null!;
+
+    public string Modelo { get; set; } = null!;
+
+    public int Precio { get; set; }
+}
